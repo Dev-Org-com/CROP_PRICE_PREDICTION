13d9be86177a3b3ea9e8c587c5415ceeff32a6b0
Justcolins <collinscheruiyot9@gmail.com>
2024-05-08 19:17:39 +0300

    counties changes

--- app.py
+++ app.py
@@ -14,14 +14,9 @@ numerical_columns = ['Supply Volume', 'usd_rate', 'Year', 'Month', 'Day']
 
 # List of counties in Kenya
 counties = [
-    "Baringo", "Bomet", "Bungoma", "Busia", "Elgeyo Marakwet", "Embu", 
-    "Garissa", "Homa Bay", "Isiolo", "Kajiado", "Kakamega", "Kericho", 
-    "Kiambu", "Kilifi", "Kirinyaga", "Kisii", "Kisumu", "Kitui", 
-    "Kwale", "Laikipia", "Lamu", "Machakos", "Makueni", "Mandera", 
-    "Marsabit", "Meru", "Migori", "Mombasa", "Murang'a", "Nairobi", 
-    "Nakuru", "Nandi", "Narok", "Nyamira", "Nyandarua", "Nyeri", 
-    "Samburu", "Siaya", "Taita Taveta", "Tana River", "Tharaka Nithi", 
-    "Trans Nzoia", "Turkana", "Uasin Gishu", "Vihiga", "Wajir", "West Pokot"
+    "Busia",  "Embu","Garissa", "Kilifi", "Kirinyaga", "Kisii", "Kisumu", "Kwale", "Migori", "Mombasa", "Nairobi", 
+    "Nakuru", "Nyandarua", "Nyeri", 
+     "Siaya", "TharakaNithi", "Uasin Gishu"
 ]
 
 markets = [
